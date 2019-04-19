63719b4002fafe3368fe92a8a5a1f5ef997d5307
Anton Smirnov <sandfox@sandfox.me>
2019-04-19 18:37:57 +0300

    Yaml dumper/loader proxy class

---
+++ src/Helpers/YamlHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SandFox\ComposerYaml\Helpers;
+
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * @internal
+ */
+class YamlHelper
+{
+    /**
+     * @param mixed $value
+     * @return string
+     */
+    public static function encode($value)
+    {
+        if (function_exists('yaml_emit')) {
+            // use ext-yaml
+            return yaml_emit($value);
+        } else {
+            // fall back to symfony/yaml
+            return Yaml::dump($value);
+        }
+    }
+
+    /**
+     * @param string $value
+     * @return mixed
+     */
+    public static function decode($value)
+    {
+        if (function_exists('yaml_parse')) {
+            // use ext-yaml
+            return yaml_parse($value);
+        } else {
+            // fall back to symfony/yaml
+            return Yaml::parse($value);
+        }
+    }
+}
